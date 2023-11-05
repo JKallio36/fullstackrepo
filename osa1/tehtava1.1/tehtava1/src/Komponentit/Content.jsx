8b98361ff688c7ee26a1e95c42464e688c4f1c40
Janne Kalliomäki <jannekalliomaki@elisanet.fi>
2023-11-05 17:49:43 +0200

    Ladataan tehtävä palautus, Tehtävä 1.2

--- osa1/tehtava1.1/tehtava1/src/Komponentit/Content.jsx
+++ osa1/tehtava1.1/tehtava1/src/Komponentit/Content.jsx
@@ -1,12 +1,14 @@
-const Content = (props) => {
+const Content = ({ content }) => {
+	const parts = content.map((part, index) => <Part key={index} part={part.part} exercises={part.exercises} />);
+
+	return <div className="Content">{parts}</div>;
+};
+
+const Part = ({ part, exercises }) => {
 	return (
-		<div className="Content">
-			{props.content.map((part, index) => (
-				<div key={index}>
-					<p>{part.part}</p>
-					<p>{part.exercises}</p>
-				</div>
-			))}
+		<div>
+			<p>Part: {part}</p>
+			<p>Exercises: {exercises}</p>
 		</div>
 	);
 };
